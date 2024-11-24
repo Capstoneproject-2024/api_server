06ed2a88eca3d71b99353a66c68baa1151fcb367
Seonu-32020 <sunwoo32016@gmail.com>
2024-11-25 00:45:02 +0900

    api server include sim-db connection test

--- api_server.py
+++ api_server.py
@@ -1,8 +1,5 @@
-from fastapi import FastAPI, Request, HTTPException, status,Depends  # FastAPI import
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Depends  # FastAPI import
 from fastapi.middleware.cors import CORSMiddleware
-from contextlib import asynccontextmanager
-import asyncio
 from MySQLConnection import MySQLConnection, lifespan, get_mysql_connection
 
 
@@ -11,7 +8,7 @@ import router.user_router as user_router
 import router.book_router as book_router
 import router.friend_router as friend_router
 import router.group_router as group_router
-
+import SimMatcher.api_db_connection as sim_tester
 
 app = FastAPI(lifespan=lifespan)
 
@@ -19,6 +16,7 @@ app.include_router(user_router.router)
 app.include_router(book_router.router)
 app.include_router(friend_router.router)
 app.include_router(group_router.router)
+app.include_router(sim_tester.router)
 
 app.add_middleware(
     CORSMiddleware,
