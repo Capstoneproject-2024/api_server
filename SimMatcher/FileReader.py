d3b18f3dd6722955205e3b5f1f32db5ead945c42
rahanaman <gr030315@gmail.com>
2024-11-26 22:17:19 +0900

    linux enviorment setting

--- SimMatcher/FileReader.py
+++ SimMatcher/FileReader.py
@@ -2,7 +2,7 @@ import csv
 import re
 import json
 import pandas as pd
-from api_db_connection import *
+from .api_db_connection import *
 from MySQLConnection import MySQLConnection, get_mysql_connection
 
 class Filereader:
@@ -89,6 +89,7 @@ class Filereader:
         return bv
 
     def get_book_name_by_id(self):
+        pass    
 
 # Others =======================================================================================================
     def exit(self):
