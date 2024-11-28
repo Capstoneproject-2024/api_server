9db30d04c6b293ab164534d8e05c00156f2f6d85
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-11-28 13:55:31 +0900

    fix($recommend) : 그룹 추천 sql 수정

--- router/recommend_router.py
+++ router/recommend_router.py
@@ -1,42 +1,49 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 from models import *
 from MySQLConnection import MySQLConnection, get_mysql_connection
-
+from collections import defaultdict
 
 router = APIRouter(prefix="/getRecommend")
 
 
 @router.get("/question_recommend")
 def get_question_recommend(
-    userID: int, questionID: int, db: MySQLConnection = Depends(get_mysql_connection)
+    questionID: int, db: MySQLConnection = Depends(get_mysql_connection)
 ):
     db.start_transaction()
     try:
         db.execute(
             f"""
-SELECT b.*
+SELECT qrb.userID ,b.*
 FROM bookTable b
 JOIN questionRecommendBookTable qrb
 ON b.id = qrb.bookID
-WHERE qrb.userID = {userID} AND qrb.questionID = {questionID};
+WHERE qrb.questionID = {questionID};
+
 
 """
         )
         dbResult = db.fetchall()
         db.commit()
-        return [
-            Book(
-                id=book[0],
-                name=book[1],
-                author=book[2],
-                publisher=book[3],
-                year=book[4],
-                desc=book[5],
-                image=book[6],
-                ISBN=book[7],
+
+        recommends = defaultdict(list)
+
+        for book in dbResult:
+            recommends[book[0]].append(
+                Book(
+                    id=book[1],
+                    name=book[2],
+                    author=book[3],
+                    publisher=book[4],
+                    year=book[5],
+                    desc=book[6],
+                    image=book[7],
+                    ISBN=book[8],
+                )
             )
-            for book in dbResult
-        ]
+
+        return recommends
+
     except Exception as e:
         # 오류 발생 시 롤백
         print(f"오류 발생: {e}")
